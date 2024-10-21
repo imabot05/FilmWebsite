9ee028576a5beb71477663ba1d3731425147770d
Nguyễn Khánh <150260593+khnhk0ogei04@users.noreply.github.com>
2024-10-22 01:22:18 +0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,14 +1,13 @@
--- Create WEB_FILM database
-CREATE DATABASE IF NOT EXISTS WEB_FILM;
-USE WEB_FILM;
+CREATE DATABASE IF NOT EXISTS WEB_FILM_DATABASE;
+USE WEB_FILM_DATABASE;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET TIME_ZONE = "+00:00";
 
-DROP TABLE IF EXISTS `CINEMAS`;
-CREATE TABLE `CINEMAS` (
+DROP TABLE IF EXISTS `cinemas`;
+CREATE TABLE `cinemas` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `cinema_name` VARCHAR(255) NOT NULL,
     `cinema_address` VARCHAR(255) NOT NULL,
@@ -53,40 +52,18 @@ CREATE TABLE `categories` (
 INSERT INTO `categories` (`category_name`) 
 VALUES ('Khoa học viễn tưởng'), ('Hành động'), ('Tâm lý - Tình cảm');
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `roleid` INT NOT NULL,
-    `username` VARCHAR(150) NOT NULL,
-    `password` VARCHAR(15) NOT NULL,
-    `fullname` VARCHAR(150) NOT NULL,
-    `avatar` LONGTEXT,
-    `email` VARCHAR(255),
-    `city` VARCHAR(255),
-    `phone` VARCHAR(12),
-    `status` INT NOT NULL DEFAULT 1,
-    `createddate` TIMESTAMP NULL,
-    `modifieddate` TIMESTAMP NULL,
-    `createdby` VARCHAR(255) NULL,
-    `modifiedby` VARCHAR(255) NULL,
-    PRIMARY KEY (`id`),
-    FOREIGN KEY (`roleid`) REFERENCES `roles`(`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
-
-INSERT INTO `users` (`id`, `roleid`, `username`, `password`, `fullname`, `avatar`, `email`, `city`, `phone`) VALUES
-(1, 1, 'khanh04', '123456', 'Nguyễn Văn A', 'http://example.com/avatar1.jpg', 'admin@example.com', 'Hà Nội', '0123456789'),
-(2, 2, 'cuong04', '123456', 'Nguyễn Văn B', 'http://example.com/avatar1.jpg', 'admin@example.com', 'Hà Nội', '0123456789');
-
 DROP TABLE IF EXISTS `movies`;
 CREATE TABLE `movies` (
     `id` INT NOT NULL AUTO_INCREMENT,
-    `movie_name` VARCHAR(250) NOT NULL,
-    `movie_category_id` INT, 
+    `movie_name` VARCHAR(255) NOT NULL,
+    `movie_category_id` INT NOT NULL, 
     `movie_description` TEXT,
     `movie_directors` VARCHAR(1000),
     `movie_cast` VARCHAR(1000),
-    `release_date` TIMESTAMP NULL,
     `running_time` TIME DEFAULT '00:00:00',
+    `image` TEXT,
+    `status` tinyint(1) default 1,
+    `trailer_url` VARCHAR(255),
     `createddate` TIMESTAMP NULL,
     `modifieddate` TIMESTAMP NULL,
     `createdby` VARCHAR(255) NULL,
@@ -95,121 +72,189 @@ CREATE TABLE `movies` (
     FOREIGN KEY (`movie_category_id`) REFERENCES `categories`(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
 
-INSERT INTO `movies` (`movie_name`, `movie_category_id`, `movie_description`, `movie_directors`, `movie_cast`, `release_date`, `running_time`) VALUES 
-('Inception', 1, 'A skilled thief is offered a chance to have his past crimes forgiven by implanting another person\'s idea into their subconscious.', 'Christopher Nolan', 'Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page', '2024-10-09 00:00:00', '02:28:00'),
-('Parasite', 2, 'A poor family schemes to become employed by a wealthy family and infiltrates their household by posing as unrelated, highly qualified individuals.', 'Bong Joon-ho', 'Song Kang-ho, Lee Sun-kyun, Cho Yeo-jeong', '2024-10-08 00:00:00', '02:12:00');
+INSERT INTO `movies` (`movie_name`, `movie_category_id`, `movie_description`, `movie_directors`, `movie_cast`, `running_time`) VALUES 
+('Inception', 1, 'A skilled thief is offered a chance to have his past crimes forgiven by implanting another person\'s idea into their subconscious.', 'Christopher Nolan', 'Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page', '02:28:00'),
+('Parasite', 2, 'A poor family schemes to become employed by a wealthy family and infiltrates their household by posing as unrelated, highly qualified individuals.', 'Bong Joon-ho', 'Song Kang-ho, Lee Sun-kyun, Cho Yeo-jeong', '02:12:00');
 
-DROP TABLE IF EXISTS `schedule`;
-CREATE TABLE `schedule` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `movie_id` INT DEFAULT NULL,
-    `cinema_id` INT DEFAULT NULL,
-    `schedule_date` DATE DEFAULT NULL,
-    `schedule_start` TIME DEFAULT NULL,
-    `schedule_end` TIME DEFAULT NULL,
+DROP TABLE IF EXISTS `discount`;
+CREATE TABLE `discount` (
+	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `movie_id` INT NOT NULL,
+    `discount_title` VARCHAR(255),
+    `discount_content` VARCHAR(255),
+    `discount_percentage` INT,
     `createddate` TIMESTAMP NULL,
     `modifieddate` TIMESTAMP NULL,
     `createdby` VARCHAR(255) NULL,
     `modifiedby` VARCHAR(255) NULL,
-    PRIMARY KEY (`id`),
-    FOREIGN KEY (`movie_id`) REFERENCES `movies`(`id`) ON DELETE CASCADE,
-    FOREIGN KEY (`cinema_id`) REFERENCES `cinemas`(`id`) ON DELETE SET NULL
+    foreign key (`movie_id`) references `movies`(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
 
-INSERT INTO `schedule` (`movie_id`, `cinema_id`, `schedule_date`, `schedule_start`, `schedule_end`) VALUES
-(1, 1, '2024-10-10', '14:00:00', '16:00:00'),
-(2, 2, '2024-10-10', '18:00:00', '20:00:00'),
-(1, 2, '2024-10-11', '20:30:00', '22:30:00');
+INSERT INTO `discount` (movie_id, discount_title, discount_content, discount_percentage)
+VALUES (1, 'DISCOUNT 1', 'ABC', 20);
+
+INSERT INTO `discount` (movie_id, discount_title, discount_content, discount_percentage)
+VALUES (2, 'DISCOUNT 2', 'XYZ', 15);
+
+DROP TABLE IF EXISTS `shifts`;
+CREATE TABLE `shifts` (
+	`id` INT NOT NULL PRIMARY KEY auto_increment,
+    `name` VARCHAR(255),
+    `shift_start` TIME,
+    `shift_end` TIME 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+INSERT INTO `shifts` (id, name, shift_start, shift_end) 
+VALUES 
+(1, 'Morning', '07:00:00', '11:00:00'),
+(2, 'Afternoon', '13:00:00', '17:00:00'),
+(3, 'Evening', '19:00:00', '23:00:00');
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `roleid` INT NOT NULL,
+    `username` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `fullname` VARCHAR(255),
+    `avatar` LONGTEXT,
+    `email` VARCHAR(255),
+    `city` VARCHAR(255),
+    `phone` VARCHAR(255),
+    `status` TINYINT default 1,
+    `account_balance` INT,
+    `order_count` INT default 0,
+    `createddate` TIMESTAMP,
+    `modifieddate` TIMESTAMP,
+    `createdby` VARCHAR(255),
+	`modifiedby` VARCHAR(255),
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`roleid`) REFERENCES `roles`(`id`) ON DELETE CASCADE
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+    
+INSERT INTO `users` (roleid, username, password, fullname, avatar, email, city, phone, status, account_balance, order_count) 
+VALUES (1, 'phuc', 'phuc', 'phuc', 'avatar1.png', 'lon@example.com', 'New York', '1234567890', 1, 150000, 0);
+
+INSERT INTO `users` (roleid, username, password, fullname, avatar, email, city, phone, status, account_balance, order_count) 
+VALUES (2, 'khanh8a04', 'khanhbkit1', 'Nguyen Duy Khanh', 'avatar2.png', 'khanhk72nh@gmail.com', 'Ha Noi', '0328873260', 1, 500000, 0);
+
+DROP TABLE IF EXISTS `schedule`;
+CREATE TABLE `schedules` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `movie_id` INT NOT NULL,
+    `cinema_id` INT NOT NULL,
+    `schedule_date` DATE NOT NULL,
+    `shift_id` INT NOT NULL,
+    `createddate` TIMESTAMP,
+    `modifieddate` TIMESTAMP,
+    `createdby` VARCHAR(255),
+    `modifiedby` VARCHAR(255),
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`movie_id`) REFERENCES `movies`(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`cinema_id`) REFERENCES `cinemas`(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`shift_id`) REFERENCES `shifts`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+INSERT INTO `schedules` (movie_id, cinema_id, schedule_date, shift_id) 
+VALUES (1, 1, '2024-10-22', 1);
+
+INSERT INTO `schedules` (movie_id, cinema_id, schedule_date, shift_id) 
+VALUES (2, 2, '2024-10-23', 1);
 
 DROP TABLE IF EXISTS `seats`;
 CREATE TABLE `seats` (
-	`id` int not null auto_increment,
-    `seat_type` varchar(50) not null,
-    `cinema_id` int default null,
-    `seat_row` varchar(2),
-    `seat_number` int,
-    `seat_status` TINYINT(1) DEFAULT 0, -- 0 = Available, 1 = Booked
-    `createddate` TIMESTAMP NULL,
-	`modifieddate` TIMESTAMP NULL,
-	`createdby` VARCHAR(255) NULL,
-	`modifiedby` VARCHAR(255) NULL,
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `seat_type` VARCHAR(255),
+    `cinema_id` INT NOT NULL,
+    `seat_row` VARCHAR(2) NOT NULL,
+    `seat_number` INT NOT NULL,
+    `createddate` TIMESTAMP,
+    `modifieddate` TIMESTAMP,
+    `createdby` VARCHAR(255),
+    `modifiedby` VARCHAR(255),
     PRIMARY KEY (`id`),
     FOREIGN KEY (`cinema_id`) REFERENCES `cinemas`(`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
 
-INSERT INTO `seats` (`seat_type`, `cinema_id`, `seat_row`, `seat_number`, `seat_status`) VALUES
-('Thường', 1 , 'A', 1, 0),
-('Thường', 1, 'A', 2, 1), 
-('VIP', 1, 'B', 1, 0),
-('VIP', 1, 'B', 2, 0);
-
-DROP TABLE IF EXISTS `booking`;
-CREATE TABLE `booking` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `userid` INT DEFAULT NULL,
-    `schedule_id` INT DEFAULT NULL,
-    `price` DOUBLE DEFAULT 0, 
-    `createddate` TIMESTAMP NULL,
-    `modifieddate` TIMESTAMP NULL,
-    `createdby` VARCHAR(255) NULL,
-    `modifiedby` VARCHAR(255) NULL,
-    PRIMARY KEY (`id`),
-    FOREIGN KEY (`userid`) REFERENCES `users`(`id`) ON DELETE CASCADE,
-    FOREIGN KEY (`schedule_id`) REFERENCES `schedule`(`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
-
-INSERT INTO `booking` (`userid`, `schedule_id`, `price`) VALUES
-(2, 1, 200000), 
-(1, 2, 120000);
+INSERT INTO `seats` (seat_type, cinema_id, seat_row, seat_number) 
+VALUES 
+('VIP', 1, 'A', 1),
+('Regular', 1, 'B', 2),
+('Regular', 1, 'C', 3),
+('VIP', 1, 'A', 2);
 
 DROP TABLE IF EXISTS `ratings`;
 CREATE TABLE `ratings` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `user_id` INT NOT NULL,
     `movie_id` INT NOT NULL,
-    `rating_value` INT NOT NULL CHECK (`rating_value` >= 1 AND `rating_value` <= 5),
-    `rating_comment` TEXT,
-    `createddate` TIMESTAMP NULL,
-    `modifieddate` TIMESTAMP NULL,
-    `createdby` VARCHAR(255) NULL,
-    `modifiedby` VARCHAR(255) NULL,
+    `rating_value` INT NOT NULL,
+    `rating_comment` VARCHAR(255),
+    `createddate` TIMESTAMP,
+    `modifieddate` TIMESTAMP,
+    `createdby` VARCHAR(255),
+    `modifiedby` VARCHAR(255),
     PRIMARY KEY (`id`),
     FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE,
     FOREIGN KEY (`movie_id`) REFERENCES `movies`(`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
 
-INSERT INTO `ratings` (`user_id`, `movie_id`, `rating_value`, `rating_comment`) VALUES
-(2, 1, 5, 'Phim rất hay'),
-(1, 2, 4, 'Như cl');
-
+INSERT INTO `ratings` (user_id, movie_id, rating_value, rating_comment) 
+VALUES 
+(1, 1, 5, 'OKE'),
+(2, 2, 4, 'cc');
 
 DROP TABLE IF EXISTS `booking_details`;
 CREATE TABLE `booking_details` (
     `id` INT NOT NULL AUTO_INCREMENT,
-    `booking_id` INT NOT NULL,
+    `user_id` INT NOT NULL,
     `seat_id` INT NOT NULL,
-    `cinema_id` INT NOT NULL,
-    `price` DOUBLE DEFAULT 0,
+    `schedule_id` INT NOT NULL,
+    `original_price` INT,
+    `discounted_price` INT,
+    `order_time` DATETIME,
+    `payment_status` TINYINT(1),
+    `createddate` TIMESTAMP,
+    `modifieddate` TIMESTAMP,
+    `createdby` VARCHAR(255),
+    `modifiedby` VARCHAR(255),
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`booking_id`) REFERENCES `booking`(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE,
     FOREIGN KEY (`seat_id`) REFERENCES `seats`(`id`) ON DELETE CASCADE,
-    FOREIGN KEY (`cinema_id`) REFERENCES `cinemas`(`id`) ON DELETE CASCADE
+    FOREIGN KEY (`schedule_id`) REFERENCES `schedules`(`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
 
-INSERT INTO `booking_details` (`booking_id`, `seat_id`, `cinema_id`, `price`) VALUES
-(1, 1, 1, 100000), 
-(1, 2, 1, 100000), 
-(2, 1, 1, 120000); 
+INSERT INTO `booking_details` (user_id, seat_id, schedule_id, original_price, discounted_price, order_time, payment_status) 
+VALUES 
+(1, 1, 1, 100000, 80000, '2024-10-22 14:30:00', 1),
+(2, 2, 2, 120000, 100000, '2024-10-23 16:00:00', 0);
+
+DROP TABLE IF EXISTS `movie_seat_prices`;
+CREATE TABLE `movie_seat_prices` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `movie_id` INT NOT NULL,
+    `seat_type` VARCHAR(255) NOT NULL,
+    `base_price` INT NOT NULL,
+    `createddate` TIMESTAMP,
+    `modifieddate` TIMESTAMP,
+    `createdby` VARCHAR(255),
+    `modifiedby` VARCHAR(255),
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`movie_id`) REFERENCES `movies`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+DROP TABLE IF EXISTS `seat_schedule_status`;
+CREATE TABLE `seat_schedule_status` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `seat_id` INT NOT NULL,
+    `schedule_id` INT NOT NULL,
+    `status` TINYINT(1) NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`seat_id`) REFERENCES `seats`(`id`),
+    FOREIGN KEY (`schedule_id`) REFERENCES `schedules`(`id`) 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
 
 
-COMMIT;
-use web_film;
 
-ALTER TABLE `movies`
-ADD COLUMN `image` varchar(255);
 
-ALTER TABLE `movies`
-ADD COLUMN `status` int;
-
-ALTER TABLE `movies`
-DROP column `release_date`;
+    
